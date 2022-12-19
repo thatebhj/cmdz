119eee3f719d591398b74255f0621bd3f76e5c29
B.H.J Thate <thatebhj@gmail.com>
2022-12-19 19:01:41 +0100

    add modz

---
+++ modz/config.py
@@ -0,0 +1,28 @@
+# This file is placed in the Public Domain
+
+
+"config"
+
+
+from cmdz.objects import last, printable, keys, edit, write
+from cmdz.running import Cfg
+
+
+def __dir__():
+    return (
+            "cfg",
+           ) 
+
+
+def cfg(event):
+    last(Cfg)
+    if not event.sets:
+        event.reply(printable(
+                              Cfg,
+                              keys(Cfg),
+                             )
+                   )
+    else:
+        edit(Cfg, event.sets)
+        write(Cfg)
+        event.done()
